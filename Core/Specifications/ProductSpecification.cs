556d5d695305e75d57aa04f191b71daa2336577e
Isaias Villegas <isaiasvillegasv@outlook.com>
2025-12-21 17:25:58 -0600

    End of section 5

--- Core/Specifications/ProductSpecification.cs
+++ Core/Specifications/ProductSpecification.cs
@@ -5,12 +5,18 @@ namespace Core.Specifications;
 
 public class ProductSpecification : BaseSpecification<Product>
 {
-  public ProductSpecification(string? brand, string? type, string? sort) : base(x =>
-    (string.IsNullOrWhiteSpace(brand) || x.Brand == brand) &&
-    (string.IsNullOrWhiteSpace(type) || x.Type == type)
+  public ProductSpecification(ProductSpecParams specParams) : base(x =>
+    (string.IsNullOrEmpty(specParams.Search) || x.Name.ToLower().Contains(specParams.Search)) &&
+    (specParams.Brands.Count == 0 || specParams.Brands.Contains(x.Brand)) &&
+    (specParams.Types.Count == 0 || specParams.Types.Contains(x.Type))
   )
   {
-   switch (sort)
+   ApplyPaging(specParams.PageSize * (specParams.PageIndex -1), specParams.PageSize);
+   // 5 * ( 1 - 1) = 0, 5
+   // 5 * ( 2 - 1) = 5, 5
+   // 5 * ( 3 - 1) = 10, 5 saltamos 10, tomamos 5
+
+   switch (specParams.Sort)
    {
     case "priceAsc":
       AddOrderBy(x => x.Price);
